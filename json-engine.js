88beeebfa8eb83a40374f9810f584117d6456120
James Mortemore <james.mortemore@clock.co.uk>
2013-11-10 11:10:56 +0000

    Initial commit

---
+++ json-engine.js
@@ -0,0 +1,235 @@
+var _ = require('lodash')
+  , emptyFn = function () {}
+  , EventEmitter = require('events').EventEmitter
+  , fs = require('fs')
+
+// Note, initial require will cause a block due to file setup (create/read)
+
+module.exports = function (file, engineOptions) {
+  var self = new EventEmitter()
+    , options = _.extend({ idProperty: 'id' }, engineOptions)
+    , data = []
+    , idIndexData = {}
+
+  try {
+    // Check if the file exists, if not, create it
+    if (!fs.existsSync(file)) {
+      fs.openSync(file, 'w')
+    } else {
+      var fileData = fs.readFileSync(file)
+
+      if (fileData.length !== 0) data = JSON.parse(fileData)
+    }
+  } catch (e) {
+    self.emit('error', e)
+    return false
+  }
+
+  // Build id index
+  data.forEach(function (object) {
+    idIndexData[object[options.idProperty]] = object
+  })
+
+  // Handle id increment
+  function getNextId() {
+    var dataIds = Object.keys(idIndexData)
+  
+    dataIds.sort(function (a, b) {
+      return b - a
+    })
+
+    if (dataIds && dataIds[0]) {
+      var id = _.parseInt(dataIds[0]) + 1
+      return id
+    } else {
+      return 1
+    }
+  }
+
+  function saveFile(emit, object, callback) {
+    fs.writeFile(file, JSON.stringify(data, null, 2), function (error) {
+      if (error) return callback(error)
+
+      if (emit) self.emit(emit, object)
+
+      callback(error, _.clone(object))
+    })
+  }
+
+
+  function create(object, callback) {
+    var createdObject = _.clone(object)
+
+    callback = callback || emptyFn
+    // if id is any falsy consider it empty
+    if (!createdObject[options.idProperty]) {
+      delete object[options.idProperty]
+    }
+    self.emit('create', createdObject)
+
+    var id = getNextId()
+
+    createdObject[options.idProperty] = id
+
+    idIndexData[id] = createdObject
+    data.push(createdObject)
+
+    saveFile('afterCreate', createdObject, callback)
+  }
+
+  function createOrUpdate(object, callback) {
+    if (typeof object[options.idProperty] === 'undefined') {
+      // Create a new object
+      self.create(object, callback)
+    } else {
+      // Try and find the object first to update
+      self.read(object[options.idProperty], function(err, entity) {
+        if (err) {
+          return callback(err)
+        }
+        if (entity) {
+          // We found the object so update
+          self.update(object, callback)
+        } else {
+          // We didn't find the object so create
+          self.create(object, callback)
+        }
+      })
+    }
+  }
+
+  function read(id, callback) {
+    self.emit('read', id)
+
+    callback = callback || emptyFn
+    var error = null
+
+    if (!idIndexData[id]) {
+      error = new Error('No object found with \'' + options.idProperty +
+          '\' = \'' + id + '\'')
+    }
+
+    callback(error, _.clone(idIndexData[id]))
+  }
+
+  function update(object, overwrite, callback) {
+    /* jshint maxcomplexity: 7 */
+    if (typeof overwrite === 'function') {
+      callback = overwrite
+      overwrite = false
+    }
+
+    self.emit('update', object, overwrite)
+    callback = callback || emptyFn
+    var updateObject = _.extend({}, object)
+      , id = object[options.idProperty]
+
+    if (id === undefined || id === null) {
+      return callback(new Error('Object has no \''
+        + options.idProperty + '\' property'))
+    }
+
+    if (!idIndexData[id]) {
+      return callback(new Error('No object found with \'' + options.idProperty +
+          '\' = \'' + id + '\''))
+    }
+
+    var updateData = overwrite ? updateObject : _.extend(idIndexData[id], updateObject)
+    idIndexData[id] = updateData
+
+    saveFile('afterUpdate', updateData, callback)
+  }
+
+
+  function updateMany() {
+    throw new Error('updateMany is not implemented for the json save engine')
+  }
+
+  function deleteMany() {
+    throw new Error('deleteMany is not implemented for the json save engine')
+  }
+
+   /**
+   * Deletes one object. Returns an error if the object can not be found
+   * or if the ID property is not present.
+   *
+   * @param {Object} object to delete
+   * @param {Function} callback
+   * @api public
+   */
+  function del(id, callback) {
+
+    callback = callback || emptyFn
+
+    if (typeof callback !== 'function') {
+      throw new TypeError('callback must be a function or empty')
+    }
+
+    if (!idIndexData[id]) {
+      return callback(new Error('No object found with \'' + options.idProperty +
+          '\' = \'' + id + '\''))
+    }
+
+    self.emit('delete', id)
+
+    delete idIndexData[id]
+
+    // Work around for dynamic property
+    var find = {}
+    find[options.idProperty] = id
+
+    data.splice(_.findIndex(data, find), 1)
+
+    saveFile('afterDelete', id, callback)
+  }
+
+  function findOne(query, options, callback) {
+    if (typeof options === 'function') {
+      callback = options
+      options = {}
+    }
+    self.emit('findOne', query)
+
+    var foundData = _.where(data, query)
+
+    if (!foundData || !foundData.length) return callback(new Error('No object found'))
+
+    callback(null, _.clone(foundData[0]))
+  }
+
+  function find(query, options, callback) {
+    if (typeof options === 'function') {
+      callback = options
+      options = {}
+    }
+    self.emit('find', query)
+
+    var foundData = _.where(data, query)
+
+    if (!foundData || !foundData.length) return callback(new Error('No objects found'))
+
+    callback(null, _.clone(foundData))
+  }
+
+  function count(query, callback) {
+    self.emit('count', query)
+    
+    self.find(query, function (error, objects) {
+      callback(null, objects ? objects.length : 0)
+    })
+  }
+
+  return _.extend(self,
+    { create: create
+    , createOrUpdate: createOrUpdate
+    , read: read
+    , update: update
+    , updateMany: updateMany
+    , deleteMany: deleteMany
+    , 'delete': del
+    , find: find
+    , findOne: findOne
+    , count: count
+    , idProperty: options.idProperty
+    })
+}
---
+++ test/json-engine.test.js
@@ -0,0 +1,199 @@
+var idProperty = 'testId'
+  , save = require('../json-engine')('test.json', { idProperty: idProperty })
+  , fixtures = require('./fixtures')
+  , should = require('should')
+  , fs = require('fs')
+  , _ = require('lodash')
+
+
+describe('#init', function () {
+  it('should setup save correctly', function () {
+    save.should.not.equal(false)
+    save.idProperty.should.equal(idProperty)
+  })
+})
+
+describe('#create', function() {
+  it('should create a new object', function (done) {
+    save.create(fixtures[0], function (error, object) {
+      should.not.exist(error)
+      should.exist(object)
+
+      object.should.have.property(idProperty)
+      object[idProperty].should.equal(1)
+
+      done()
+    })
+  })
+
+  it('should auto increment the id', function (done) {
+    save.create(fixtures[1], function (error, object) {
+      should.not.exist(error)
+      should.exist(object)
+
+      object.should.have.property(idProperty)
+      object[idProperty].should.equal(2)
+
+      done()
+    })
+  })
+})
+
+describe('#read', function () {
+  it('should find an object', function (done) {
+    save.read(1, function (error, object) {
+      should.not.exist(error)
+      should.exist(object)
+
+      object.should.have.property(idProperty)
+      object[idProperty].should.equal(1)
+
+      done()
+    })
+  })
+})
+
+describe('#update', function () {
+  it('should update an object', function (done) {
+    save.read(1, function (error, object) {
+      should.not.exist(error)
+      should.exist(object)
+
+      object.should.have.property(idProperty)
+      object[idProperty].should.equal(1)
+
+      object['test'] = 'asd'
+      object[idProperty]++
+
+      save.update(object, function (err, updatedObject) {
+        should.not.exist(err)
+        should.exist(updatedObject)
+
+        updatedObject.should.have.property('test')
+
+        // Ensure we aren't being passed a reference
+        updatedObject.should.not.equal(object)
+
+        done()
+      })
+
+
+    })
+  })
+})
+
+describe('#updateMany', function () {
+  it('should throw an error', function () {
+    try {
+      save.updateMany(fixtures[0])
+    } catch(e) {
+      e.message.should.equal('updateMany is not implemented for the json save engine')
+    }
+  })
+})
+
+describe('#deleteMany', function () {
+  it('should throw an error', function () {
+    try {
+      save.deleteMany(fixtures[0])
+    } catch(e) {
+      e.message.should.equal('deleteMany is not implemented for the json save engine')
+    }
+  })
+})
+
+describe('#count', function () {
+  it('should return the number of objects found', function (done) {
+    save.count({ foo: 'bar' }, function (error, count) {
+      should.not.exist(error)
+      count.should.equal(2)
+
+      done()
+    })
+  })
+})
+
+describe('#find', function () {
+  it('should find objects', function (done) {
+    save.find({ foo: 'bar' }, function (error, objects) {
+      should.not.exist(error)
+
+      objects.length.should.equal(2)
+
+      _.each(objects, function (object) {
+        object.foo.should.equal('bar')
+      })
+
+      done()
+    })
+  })
+})
+
+describe('#findOne', function () {
+  it('should find objects', function (done) {
+    save.findOne({ foo: 'bar' }, function (error, object) {
+      should.not.exist(error)
+
+      _.isArray(object).should.equal(false)
+      _.isObject(object).should.equal(true)
+
+      object.foo.should.equal('bar')
+
+      done()
+    })
+  })
+})
+
+describe('#createOrUpdate', function() {
+  it('should create a new object', function (done) {
+    save.createOrUpdate(fixtures[1], function (error, object) {
+      should.not.exist(error)
+      should.exist(object)
+
+      object.should.have.property(idProperty)
+      object[idProperty].should.equal(3)
+
+      done()
+    })
+  })
+
+  it('should update an object', function (done) {
+    save.read(3, function (error, object) {
+      object['foo'] = 'nobar'
+      save.createOrUpdate(object, function (error, updatedObject) {
+        should.not.exist(error)
+        should.exist(updatedObject)
+
+        updatedObject.should.have.property('foo')
+        updatedObject.foo.should.equal('nobar')
+        updatedObject[idProperty].should.equal(3)
+        updatedObject.should.not.equal(object)
+
+        done()
+      })
+    })
+  })
+})
+
+describe('#delete', function () {
+  it('should delete an object', function (done) {
+    save.delete(2, function (error, id) {
+      should.not.exist(error)
+      should.exist(id)
+      id.should.equal(2)
+
+
+      save.read(2, function (err, object) {
+        err.message.should.equal('No object found with \'' + idProperty + '\' = \'2\'')
+        should.strictEqual(object, undefined)
+
+        done()
+      })
+
+    })
+  })
+})
+
+after(function() {
+  fs.unlinkSync('test.json')
+})
